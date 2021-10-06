64f991f459244bf8a08ab444d3335ca294c19c6e
echizenya_yota <virtualmisso30@gmail.com>
2021-10-06 14:33:19 +0900

    4.3.2

--- src/App.js
+++ src/App.js
@@ -4,25 +4,44 @@ import axios from "axios";
 export default function App() {
   // APIから取得したデータ
   const [items, setItems] = useState([]);
+  // input に入力した値
+  const [inputValue, setInputValue] = useState("React");
+  // 外部 APIにリクエストするときに付与するクエリパラメータ
+  const [query, setQuery] = useState(inputValue);
   // ローディング状態
   const [isLoading, setIsLoading] = useState(false);
-  const url = "https://hn.algolia.com/api/v1/search?query=react";
 
+  // 外部APIからデータを取得し、stateを更新する副作用
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
 
-      const result = await axios.get(url);
+      const result = await axios(
+        `https://hn.algolia.com/api/v1/search?query=${query}`
+      );
 
       setItems(result.data.hits);
       setIsLoading(false);
     };
 
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setQuery(inputValue);
+        }}
+      >
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button type="submit">検索</button>
+      </form>
       {isLoading ? (
         <p>Loading</p>
       ) : (
