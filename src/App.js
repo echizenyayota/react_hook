850f332b22251e6638deb812668a9b7e9eec0195
echizenya_yota <virtualmisso30@gmail.com>
2021-10-07 15:21:24 +0900

    6.3.4_2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { useState, useCallback } from "react";
 
-const Child = ({ handleClick }) => {
-  console.log("render child");
-  return <button onClick={handleClick}>Child</button>;
-};
-
 export default function App() {
   console.log("render App");
 
   const [count, setCount] = useState(0);
   const handleClick = useCallback(() => {
-    console.log("click");
+    console.log("memolized callback");
   }, []);
   return (
     <>
       <p>Counter: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
-      <Child handleClick={handleClick} />
+      <button onClick={handleClick}>logging</button>
     </>
   );
 }
