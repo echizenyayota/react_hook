12a627851cea19f016aa042be039599f32a1d822
echizenya_yota <virtualmisso30@gmail.com>
2021-10-07 15:07:04 +0900

    6.2.3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 
-const Child = React.memo(({ count }) => {
-  let i = 0;
-  while (i < 1000000000) {
-    console.log("render child");
-    return <p>Child: {count}</p>;
-  }
+const Child = React.memo(({ handleClick }) => {
+  console.log("render child");
+  return <button onClick={handleClick}>Child</button>;
 });
 
 export default function App() {
   console.log("render App");
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
 
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    console.log("click");
+  };
   return (
     <>
-      <button onClick={() => setCount1(count1 + 1)}>countup App count</button>
-      <button onClick={() => setCount2(count2 + 1)}>countup Child count</button>
-      <p>App : {count1}</p>
-      <Child count={count2} />
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <Child handleClick={handleClick} />
     </>
   );
 }
